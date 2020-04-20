d26a83d375de155f2c356eb6a557908fcb8b2153
James Smith <james.smith@djalbat.com>
2020-04-20 16:57:37 +0100

    Simplified the runtime configuration.

--- bin/example/constants.js
+++ bin/example/constants.js
@@ -4,6 +4,8 @@ const IMAGE_MAP_URI = "/imageMap",
       INDEX_PAGE_URI = "/",
       OVERLAY_IMAGE_SIZE = 128,
       INDEX_PAGE_FILE_PATH = "/index.html",
+      IMAGE_DIRECTORY_PATH = "image",
+      TEMPLATE_DIRECTORY_PATH = "template",
       TEXT_HTML_CHARSET_UTF8_CONTENT_TYPE = "text/html; charset=utf-8";
 
 module.exports = {
@@ -11,5 +13,7 @@ module.exports = {
   INDEX_PAGE_URI,
   OVERLAY_IMAGE_SIZE,
   INDEX_PAGE_FILE_PATH,
+  IMAGE_DIRECTORY_PATH,
+  TEMPLATE_DIRECTORY_PATH,
   TEXT_HTML_CHARSET_UTF8_CONTENT_TYPE
 };
--- bin/example/routes.js
+++ bin/example/routes.js
@@ -5,25 +5,25 @@ const xglServer = require("../../index"), ///
 
 const constants = require("./constants");
 
-const { templateUtilities, miscellaneousUtilities } = necessary,
-      { rc } = miscellaneousUtilities,
+const { templateUtilities } = necessary,
       { parseFile } = templateUtilities,
       { imageMapPNG, imageMapJSON } = xglServer,
-      { IMAGE_MAP_URI, OVERLAY_IMAGE_SIZE, INDEX_PAGE_FILE_PATH, TEXT_HTML_CHARSET_UTF8_CONTENT_TYPE } = constants;
+      { IMAGE_MAP_URI, OVERLAY_IMAGE_SIZE, INDEX_PAGE_FILE_PATH, IMAGE_DIRECTORY_PATH, TEMPLATE_DIRECTORY_PATH, TEXT_HTML_CHARSET_UTF8_CONTENT_TYPE } = constants;
 
 function imageMap(request, response) {
   const names = namesFromRequest(request),
-        { imageDirectoryPath } = rc,
-        overlayImageSize = OVERLAY_IMAGE_SIZE;
+        overlayImageSize = OVERLAY_IMAGE_SIZE,
+        imageDirectoryPath  = IMAGE_DIRECTORY_PATH;
 
   imageMapPNG(names, imageDirectoryPath, overlayImageSize, response);
 }
 
 function indexPage(request, response) {
   const names = namesFromRequest(request),
-        { imageDirectoryPath, templateDirectoryPath } = rc,
+        overlayImageSize = OVERLAY_IMAGE_SIZE,
         indexPageFilePath = INDEX_PAGE_FILE_PATH,
-        overlayImageSize = OVERLAY_IMAGE_SIZE;
+        imageDirectoryPath = IMAGE_DIRECTORY_PATH,
+        templateDirectoryPath = TEMPLATE_DIRECTORY_PATH;
 
   imageMapJSON(names, imageDirectoryPath, overlayImageSize, function (imageMapJSON) {
     imageMapJSON = JSON.stringify(imageMapJSON, null, "  ");
